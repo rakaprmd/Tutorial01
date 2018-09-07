8e2e547bb0268bc3d68851b94d7397b26937490b
@Ronnavelly <ronnavelly@gmail.com>
2018-09-07 18:02:54 +0700

    Initial Commit Mahasiswa.java

---
+++ Mahasiswa.java
@@ -0,0 +1,22 @@
+public class Mahasiswa{
+	
+	String nama = "";
+	int npm;
+	
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+	public String getNama() {
+		return nama
+	}
+	public void setNpm(int npm){
+		this.npm = npm;
+	}
+	public int getNpm(){
+		return npm;
+	}
+	
+	
+	
+	
+}
